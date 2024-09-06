79a039ec16da86ab8910cb30871ef9e6a94e1533
eduartev93 <161385400+eduartev93@users.noreply.github.com>
2024-09-05 21:08:13 -0500

    :+1:  Mejora En Api 20240905

--- E2 - Model Deployment in Used Vehicle Price Prediction/model_deployment/app.py
+++ E2 - Model Deployment in Used Vehicle Price Prediction/model_deployment/app.py
@@ -4,14 +4,14 @@ from flask import Flask
 from flask_restx import Api, Resource, fields
 import Model_Dev
 
-app = Flask(__name__)
+app_post = Flask(__name__)
 api = Api(
-    app,
+    app_post,
     version='1.0',
     title='Predecir el Precio de Coches API - POST',
     description='API para predecir el precio de coches')
 
-ns = api.namespace('predict', description='Precio regresión')
+ns_post = api.namespace('predict', description='Precio regresión')
 
 # Definir el modelo del cuerpo de la solicitud
 input_model = api.model('InputModel', {
@@ -29,31 +29,30 @@ resource_fields = api.model('Resource', {
 
 @ns.route('/')
 class PredictPrice(Resource):
-    @api.expect(parser)
+    @api.expect(input_model)
     @api.marshal_with(resource_fields)
-    def get(self):
-        # Obtener los parámetros de la URL
-        args = parser.parse_args()
-        mileage = args['Mileage']
-        year = args['Year']
-        state = args['State']
-        model_name = args['Model']
-        make = args['Make']
+    def post(self):
+        # Obtener los datos del cuerpo de la solicitud
+        data = request.json
+        
+        # Verificar que todos los campos necesarios estén presentes
+        if not all(key in data for key in ('Mileage', 'Year', 'State', 'Model', 'Make')):
+            return {"message": "Missing fields in request body"}, 400
 
+        # Extraer los valores
+        mileage = data['Mileage']
+        year = data['Year']
+        state = data['State']
+        model_name = data['Model']
+        make = data['Make']
+     
         # Realizar la predicción
-        predicted_price, predicted_price_std = Model_Dev.predict_price(
-            mileage=mileage, 
-            year=year, 
-            state=state, 
-            model=model_name,  # Usar model_name en lugar de model
-            make=make
-        )
+        predicted_price,predicted_price_std = predict_price(mileage=mileage, year=year, state=state, model=model_name, make=make)
 
         return {
-            "predicted_price": predicted_price,
+            'predicted_price': predicted_price,
             'predicted_price_std': predicted_price_std
-        }, 200
-
+        },200
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
